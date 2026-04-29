fec16db3cb8e8ae549f7788874fe38ac77616e5e
PEARLINS\jeff.cargill <jeff.cargill@pearlcompanies.com>
2026-04-29 11:34:07 -0500

    Add ChinookFramework project

---
+++ chinookframework/Controllers/ChinookController.cs
@@ -0,0 +1,22 @@
+﻿using chinookframework.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace chinookframework.Controllers
+{
+    public class ChinookController: Controller
+    {
+        static readonly HttpClient client = new HttpClient();
+
+        public async Task<ActionResult> Index()
+        {
+            var response = await client.GetStringAsync(Environment.GetEnvironmentVariable("CHINOOKAPIURL"));
+            var artists = JsonSerializer.Deserialize<IEnumerable<Artist>>(response);
+            return View(artists);
+        }
+    }
+}
---
+++ chinookframework/Models/Artist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace chinookframework.Models
+{
+    public class Artist
+    {
+        public int artistId { get; set; }
+        public string name { get; set; }   
+    }
+}
